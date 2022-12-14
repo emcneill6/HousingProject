a7ba7584edf7ed1f717a47e69b1edd3d45f7fed7
emcneill6 <114413814+emcneill6@users.noreply.github.com>
2022-12-14 07:19:24 -0600

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
 username = 'root' #for mysql it is root
-password = 'lucky' #this is the password you have for mysql
+password = '' #this is the password you have for mysql
 hostname = 'localhost'
 database = 'housing_project' #tells which database to use
